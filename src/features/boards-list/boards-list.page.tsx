5edd691dd4e0c94e13f49921bd0f27ec96efe9d0
Vladimir_Bavtenko <batonich84@mail.ru>
2025-11-20 20:24:19 +0300

    chor: Add config for typing env variables

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,10 +1,13 @@
 import { href, Link } from 'react-router-dom'
 import { ROUTES } from '@/shared/model/routes.ts'
+import { CONFIG } from '@/shared/model/config.ts'
 
 function BoardsListPage() {
   return (
     <div>
       <h1>Boards list</h1>
+
+      <h2>{CONFIG.API_BASE_URL}</h2>
 
       <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board 1</Link>
     </div>
